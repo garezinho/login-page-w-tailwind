bae602687f1dd2be3cae3b8cd196a1638587f17f
Guilherme Gare <gui10.gare@gmail.com>
2024-01-18 03:50:28 -0300

    Login, signup buttons, dark/light button (funcional) two workers card

---
+++ src/script.js
@@ -0,0 +1,41 @@
+function darkLight() {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Obter referência para o botão e o elemento alvo
+        var toggleButton = document.getElementById("toggleButton");
+        const targetElement = document.getElementById("bodyCg");
+
+
+
+        // Adicionar um ouvinte de evento ao botão
+        toggleButton.addEventListener("click", function () {
+            function black() {
+                targetElement.classList.remove("bg-gray-800");
+                targetElement.classList.add("bg-white");
+                toggleButton.textContent = "Escuro";
+                toggleButton.classList.add("bg-white");
+                toggleButton.classList.add("bg-gray-800");
+                toggleButton.classList.add("text-white");
+
+            }
+
+            function light() {
+                targetElement.classList.remove("bg-white");
+                targetElement.classList.add("bg-gray-800");
+                toggleButton.textContent = "Claro";
+                toggleButton.classList.remove("bg-gray-800");
+                toggleButton.classList.remove("text-white");
+                toggleButton.classList.add("bg-white");
+
+            }
+            // Alternar entre as classes e o texto do botão
+            if (targetElement.classList.contains("bg-white")) {
+                light();
+            } else {
+                black();
+            }
+        });
+    });
+
+}
+
+darkLight();
